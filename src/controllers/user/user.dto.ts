fafb7e95c86090aa548302973829366d24c4000c
dwlee-newword <dwlee.newword@gmail.com>
2024-09-27 11:11:35 +0900

    Add user controller and DTOs

---
+++ src/controllers/user/user.dto.ts
@@ -0,0 +1,59 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
+
+export class CreateUserDto {
+  @ApiProperty({
+    description: 'The email of the User',
+    type: String,
+    example: 'test@test.com',
+  })
+  @IsString()
+  @IsNotEmpty()
+  email: string;
+  @ApiProperty({
+    description: 'The name of the User',
+    type: String,
+    example: 'John Doe',
+  })
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+}
+
+export class UserDto {
+  @ApiProperty({
+    description: 'The id of the User',
+    type: Number,
+    example: 1,
+  })
+  @IsNumber()
+  @IsNotEmpty()
+  id: number;
+
+  @ApiProperty({
+    description: 'The email of the User. It can nullable',
+    type: String,
+    example: 'test@test.com',
+  })
+  @IsString()
+  email: string;
+
+  @ApiProperty({
+    description: 'The name of the User. It can nullable',
+    type: String,
+    example: 'John Doe',
+  })
+  @IsString()
+  name: string;
+}
+
+export class UserIdDto {
+  @ApiProperty({
+    description: 'The id of the User',
+    type: Number,
+    example: 1,
+  })
+  @IsNumber()
+  @IsNotEmpty()
+  id: number;
+}
--- src/modules/user.module.ts
+++ src/modules/user.module.ts
@@ -1,12 +1,22 @@
 import { Module } from '@nestjs/common';
 import { PrismaService } from '../services/prisma.service';
-import { UserController } from '../controllers/user.controller';
 import { ConfigService } from '@nestjs/config';
 import { UserService } from '../services/user.service';
+import { UserController } from '../controllers/user/user.controller';
+import { BookController } from '../controllers/book/book.controller';
+import { RentController } from '../controllers/rent/rent.controller';
+import { BookService } from '../services/book.service';
+import { RentService } from '../services/rent.service';
 
 @Module({
   imports: [],
-  controllers: [UserController],
-  providers: [PrismaService, ConfigService, UserService],
+  controllers: [UserController, BookController, RentController],
+  providers: [
+    PrismaService,
+    ConfigService,
+    UserService,
+    BookService,
+    RentService,
+  ],
 })
 export class UserModule {}
